db4e9c36ca0369880a485708cb648bf0ccac267d
Sergi Fuentes <sfuentespr@gmail.com>
2025-11-28 12:51:11 +0100

    added except

--- app.py
+++ app.py
@@ -48,5 +48,8 @@ def predict():
             "confidence": confidence
         })
     
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True) #¡¡¡¡CAMBIAR DEBUG A FALSE EN PRODUCCION!!!!
