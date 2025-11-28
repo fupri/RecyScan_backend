82685dee8cafea1f218150a23260f26a1628888d
Sergi Fuentes <sfuentespr@gmail.com>
2025-11-28 12:14:51 +0100

    fix Flask API tabulations

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def predict():
             "category": predicted_class_name,
             "confidence": confidence
         })
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
